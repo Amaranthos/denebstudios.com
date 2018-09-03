882630427ea2990d0bf573a1a647bf0133bb83c1
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2018-09-03 20:53:18 +1000

    Add firebase config

---
+++ src/fire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var config = {
+	apiKey: "AIzaSyDdsuPFjR6kZcdaY56ejzGMQOVL7CC0siU",
+	authDomain: "denebstudios-com.firebaseapp.com",
+	databaseURL: "https://denebstudios-com.firebaseio.com",
+	projectId: "denebstudios-com",
+	storageBucket: "denebstudios-com.appspot.com",
+	messagingSenderId: "233059675477"
+};
+
+export const fire = firebase.initizlizeApp(config);
+export const database = firebase.database();
+export const storage = firebase.storage();
